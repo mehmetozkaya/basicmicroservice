9fd926589dd8d4708dc95f054c7d35451ba913d0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-04-04 15:03:10 +0300

    rabbit initial

--- basicmicroservice/src/Basket/Basket.API/Controllers/BasketController.cs
+++ basicmicroservice/src/Basket/Basket.API/Controllers/BasketController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
@@ -13,6 +14,7 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        //private readonly IEventBus _eventBus;
         private readonly ILogger<BasketController> _logger;
 
         public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
@@ -42,6 +44,45 @@ namespace Basket.API.Controllers
         public async Task DeleteBasketByIdAsync(string id)
         {
             await _repository.DeleteBasketAsync(id);
+        }
+
+        [Route("checkout")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> CheckoutAsync([FromBody]BasketCheckout basketCheckout)
+        {
+            var userId = "swn"; // _identityService.GetUserIdentity();
+
+            basketCheckout.RequestId = Guid.NewGuid();
+
+            var basket = await _repository.GetBasketAsync(userId);
+
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            //var userName = this.HttpContext.User.FindFirst(x => x.Type == ClaimTypes.Name).Value;
+
+            //var eventMessage = new UserCheckoutAcceptedIntegrationEvent(userId, userName, basketCheckout.City, basketCheckout.Street,
+            //    basketCheckout.State, basketCheckout.Country, basketCheckout.ZipCode, basketCheckout.CardNumber, basketCheckout.CardHolderName,
+            //    basketCheckout.CardExpiration, basketCheckout.CardSecurityNumber, basketCheckout.CardTypeId, basketCheckout.Buyer, basketCheckout.RequestId, basket);
+
+            //// Once basket is checkout, sends an integration event to
+            //// ordering.api to convert basket to order and proceeds with
+            //// order creation process
+            //try
+            //{
+            //    _eventBus.Publish(eventMessage);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "ERROR Publishing integration event: {IntegrationEventId} from {AppName}", eventMessage.Id, "asd");
+            //    throw;
+            //}
+
+            return Accepted();
         }
 
     }
